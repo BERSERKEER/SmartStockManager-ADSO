8ae9169fb62e8a2a72d259aacccff9d600049c20
Johan <calderonjohan604@gmail.com>
2025-02-26 00:57:10 -0500

    mvc

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
-require_once '../app/core/Router.php';
-require_once '../app/core/Controller.php';
+
+require_once "../app/core/router.php";
+require_once "../app/core/controller.php";
 
 $router = new Router();
 $router->dispatch();
